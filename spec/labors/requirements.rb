9ac2511f4b86f0a30c6cdcbd9711ca936c4ebbae
Saurabh Asthana <saurabh.asthana@ucsf.edu>
2018-11-25 20:50:11 +0000

    use requirements for labors project

---
+++ spec/labors/requirements.rb
@@ -0,0 +1,9 @@
+require_relative 'models/codex'
+require_relative 'models/aspect'
+require_relative 'models/labor'
+require_relative 'models/monster'
+require_relative 'models/prize'
+require_relative 'models/project'
+require_relative 'models/victim'
+
+require_relative 'metrics/labor_metrics'
